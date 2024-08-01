ed9cfa3d5d10cffd0ff1d1881600b1e6bbfae49c
sreenivasulum <sreea568@gmail.com>
2024-08-01 15:47:37 +0530

    Update api.py

--- api.py
+++ api.py
@@ -181,7 +181,7 @@ def getRecommendation():
 
     #return job1.result
    
-    return jsonify({"job_id": job.id}), 202
+    return jsonify({"job_id": job1.id}), 202
 
 @app.route('/jobStatus/<job_id>', methods=['GET'])
 def job_status(job_id):
